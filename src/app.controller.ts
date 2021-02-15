6f88cdb1a9bc5e105031a433db7b53ecbfc7bd86
Santhosh <santhoshprogrammer94@gmail.com>
2021-02-15 16:24:00 +0530

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
+import { ExtendedMessage, RMQMessage, RMQRoute, RMQService } from 'nestjs-rmq';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly rmqService: RMQService) {}
 
   @Get()
   getHello(): string {
+    this.rmqService.notify<string>('info.none', 'My data');
     return this.appService.getHello();
   }
+
+  @RMQRoute('info.none')
+	info(data: string) {
+		console.log(data);
+	}
+  
+  @RMQRoute('info.none', { manualAck: true })
+	myOtherMethod(data: string, @RMQMessage msg: ExtendedMessage) {
+		// Any logic goes here
+		this.rmqService.nack(msg);
+		// Any logic goes here
+	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,24 @@ import { CommonModule } from './common/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './infra/database/product/product.entity';
 import { SnakeNamingStrategy } from './common/snake-naming.strategy';
+import { RMQModule } from 'nestjs-rmq';
 
 @Module({
   imports: [
     CommonModule,
     UsecasesModule,
     DatabaseModule,
+	RMQModule.forRoot({
+		exchangeName: 'exchange1',
+		connections: [
+			{
+				login: 'guest',
+				password: 'guest',
+				host: 'localhost',
+			},
+		],
+		queueName: 'my-service-queue',
+	}),
     TypeOrmModule.forRoot({
 			type: 'mysql',
 			host: 'localhost',
